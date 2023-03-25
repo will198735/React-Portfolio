64e20c40f63c5e1a32b6e8448110af9c92e236c2
narizon35 <recinoswilfredo@gmail.com>
2023-03-24 23:03:57 -0700

    setted up Resume, and Contact files

--- my-react-app/src/components/pages/Contact.js
+++ my-react-app/src/components/pages/Contact.js
@@ -1,9 +1,27 @@
+
 import React from 'react';
 
 export default function Contact() {
   return (
     <div>
       <h1>Contact</h1>
+      <div class="col-auto">
+    <label class="visually-hidden" for="autoSizingInput">Name</label>
+    <input type="text" class="form-control" id="autoSizingInput" placeholder="Name"></input>
+  </div>
+      <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address:</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder=""></input>
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">message:</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+<div class="col-auto">
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
+    
+
      
     </div>
   );
--- my-react-app/src/components/pages/Resume.js
+++ my-react-app/src/components/pages/Resume.js
@@ -3,7 +3,30 @@ import React from 'react';
 export default function Resume() {
   return (
     <div>
+      <section> 
       <h1>Resume</h1>
+      <h6> Download my resume</h6>
+      <h2>Front-end Proficiencies</h2>
+      <ul> 
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>JQuerry</li>
+        <li>Responsive Design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+      <h2>Back-end Proficiencies</h2>
+      <ul>
+      <li>APIs</li>
+      <li>Node</li>
+      <li>Exxpress</li>
+      <li>MySQL, Sequelize</li>
+
+      </ul>
+
+
+      </section>
   
     </div>
   );
