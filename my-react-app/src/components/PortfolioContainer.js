2802382729bdd46ff203bffd8141d5432244d7b8
will198735 <recinoswilfredo@gmail.com>
2023-03-26 10:17:51 -0700

    added Footer

--- my-react-app/src/components/PortfolioContainer.js
+++ my-react-app/src/components/PortfolioContainer.js
@@ -4,22 +4,31 @@ import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
+import Footer from './pages/Footer';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('AboutMe');
 
  
   const renderPage = () => {
+   
     if (currentPage === 'AboutMe') {
       return <AboutMe/>;
     }
+   
     if (currentPage === 'Portfolio') {
       return <Portfolio/>;
     }
     if (currentPage === 'Resume') {
       return <Resume />;
     }
-    return <Contact />;
+    if (currentPage === 'Contact') {
+
+      return <Contact/>;
+    }
+  
+    return <Footer/>;
+    
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
@@ -30,6 +39,7 @@ export default function PortfolioContainer() {
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       
       {renderPage()}
+      <Footer currentPage={currentPage} handlePageChange={handlePageChange} />
     </div>
   );
 }
